baa5f9cab3be3177c3d3a1934842ccd5eb39fbb2
Laszlo Teveli <tevelee@gmail.com>
2021-12-18 16:06:59 +0100

    2021 Day18

---
+++ AdventOfCode/2021/AoC_2021_Day18.swift
@@ -0,0 +1,159 @@
+import Algorithms
+import Foundation
+import Parsing
+
+public final class AoC_2021_Day18 {
+    let lines: Lines
+
+    public init(_ inputFileURL: URL) {
+        lines = inputFileURL.lines.eraseToAnyAsyncSequence()
+    }
+
+    public init(_ input: String) {
+        lines = input.lines.async.eraseToAnyAsyncSequence()
+    }
+
+    enum Node: Equatable {
+        case number(Int)
+        indirect case pair(left: Node, right: Node)
+    }
+
+    public func solvePart1() async throws -> Int {
+        try await lines.map(parse).reduce(add)!.magnitude
+    }
+
+    public func solvePart2() async throws -> Int {
+        let values = try await lines.map(parse).collect()
+        return product(values, values)
+            .lazy
+            .filter { $0 != $1 }
+            .map(add)
+            .map(\.magnitude)
+            .max()!
+    }
+
+    private func parse(line: String) -> Node {
+        nodeParser().parse(line)!
+    }
+
+    private func nodeParser() -> AnyParser<Substring, Node> {
+        Int.parser()
+            .map(Node.number)
+            .orElse(
+                "["
+                    .take(Lazy { self.nodeParser() })
+                    .skip(",")
+                    .take(Lazy { self.nodeParser() })
+                    .skip("]")
+                    .map(Node.pair)
+            )
+            .eraseToAnyParser()
+    }
+
+    private func add(left: Node, right: Node) -> Node {
+        reduce(.pair(left: left, right: right))
+    }
+
+    private func reduce(_ node: Node) -> Node {
+        var result = node
+        while let iteration = result.explode() ?? result.split() {
+            result = iteration
+        }
+        return result
+    }
+}
+
+extension AoC_2021_Day18.Node: CustomStringConvertible {
+    var magnitude: Int {
+        switch self {
+            case let .number(value):
+                return value
+            case let .pair(left: left, right: right):
+                return left.magnitude * 3 + right.magnitude * 2
+        }
+    }
+
+    var description: String {
+        switch self {
+            case let .number(value):
+                return String(value)
+            case let .pair(left: left, right: right):
+                return "(\(left),\(right))"
+        }
+    }
+
+    func split() -> Self? {
+        switch self {
+            case let .number(value):
+                if value >= 10 {
+                    let floor = Int(floor(Double(value) / 2.0))
+                    let ceil = Int(ceil(Double(value) / 2.0))
+                    return .pair(left: .number(floor), right: .number(ceil))
+                } else {
+                    return nil
+                }
+            case let .pair(left: left, right: right):
+                if let leftSplit = left.split() {
+                    return .pair(left: leftSplit, right: right)
+                } else if let rightSplit = right.split() {
+                    return .pair(left: left, right: rightSplit)
+                } else {
+                    return nil
+                }
+        }
+    }
+
+    func explode() -> Self? {
+        self.explode(depth: 0)?.value
+    }
+
+    private func explode(depth: Int) -> (value: Self, left: Int, right: Int)? {
+        if depth >= 4, case let .pair(.number(left), .number(right)) = self {
+            return (.number(0), left, right)
+        }
+        switch self {
+            case .number:
+                return nil
+            case let .pair(left: left, right: right):
+                if let explodeLeft = left.explode(depth: depth + 1) {
+                    if explodeLeft.right != 0, let rightValue = right.addToLeftMostNumber(explodeLeft.right) {
+                        return (.pair(left: explodeLeft.value, right: rightValue), explodeLeft.left, 0)
+                    } else {
+                        return (.pair(left: explodeLeft.value, right: right), explodeLeft.left, explodeLeft.right)
+                    }
+                } else if let explodeRight = right.explode(depth: depth + 1) {
+                    if explodeRight.left != 0, let leftValue = left.addToRightMostNumber(explodeRight.left) {
+                        return (.pair(left: leftValue, right: explodeRight.value), 0, explodeRight.right)
+                    } else {
+                        return (.pair(left: left, right: explodeRight.value), explodeRight.left, explodeRight.right)
+                    }
+                } else {
+                    return nil
+                }
+        }
+    }
+
+    private func addToLeftMostNumber(_ value: Int) -> Self? {
+        switch self {
+            case let .pair(left: left, right: right):
+                if let result = left.addToLeftMostNumber(value) {
+                    return .pair(left: result, right: right)
+                }
+                return nil
+            case let .number(number):
+                return .number(number + value)
+        }
+    }
+
+    private func addToRightMostNumber(_ value: Int) -> Self? {
+        switch self {
+            case let .pair(left: left, right: right):
+                if let result = right.addToRightMostNumber(value) {
+                    return .pair(left: left, right: result)
+                }
+                return nil
+            case let .number(number):
+                return .number(number + value)
+        }
+    }
+}
--- AdventOfCodeTests/2021/2021Tests.swift
+++ AdventOfCodeTests/2021/2021Tests.swift
@@ -583,4 +583,33 @@ final class AoC_2021_Tests: XCTestCase {
         XCTAssertEqual(part2, 3282)
         print("Day17 / Part2 solution: \(part2)")
     }
+
+    func test_day18_intro() async throws {
+        let problem = AoC_2021_Day18("""
+        [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+        [[[5,[2,8]],4],[5,[[9,9],0]]]
+        [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+        [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+        [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+        [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+        [[[[5,4],[7,7]],8],[[8,3],8]]
+        [[9,3],[[9,9],[6,[4,9]]]]
+        [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+        [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+        """)
+        let part1 = try await problem.solvePart1()
+        XCTAssertEqual(part1, 4140)
+        let part2 = try await problem.solvePart2()
+        XCTAssertEqual(part2, 3993)
+    }
+
+    func test_day18() async throws {
+        let problem = try AoC_2021_Day18(Resources.url(for: "2021_day18"))
+        let part1 = try await problem.solvePart1()
+        XCTAssertEqual(part1, 4641)
+        print("Day18 / Part1 solution: \(part1)")
+        let part2 = try await problem.solvePart2()
+        XCTAssertEqual(part2, 4624)
+        print("Day18 / Part2 solution: \(part2)")
+    }
 }
