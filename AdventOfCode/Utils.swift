4aae8c26a4b42fb310d69ee5a3d75093148dfbc7
Laszlo Teveli <tevelee@gmail.com>
2021-12-19 17:16:25 +0100

    2021 Day19

--- AdventOfCode/Utils.swift
+++ AdventOfCode/Utils.swift
@@ -36,4 +36,28 @@ extension Collection {
     func sorted<T>(by selector: (Element) -> T, comparator: (T, T) -> Bool) -> [Element] where T: Comparable {
         sorted { comparator(selector($0), selector($1)) }
     }
+
+    var headAndTail: (head: Element, tail: SubSequence)? {
+        guard let head = first else { return nil }
+        return (head, dropFirst())
+    }
+}
+
+extension Sequence {
+    var headAndTail: (head: Element, tail: IteratorSequence<Iterator>)? {
+        var iterator = makeIterator()
+        guard let head = iterator.next() else { return nil }
+        let tail = IteratorSequence(iterator)
+        return (head, tail)
+    }
+}
+
+extension Array where Element: Hashable {
+    func removingDuplicates() -> [Element] {
+        var addedDict = [Element: Bool]()
+
+        return filter {
+            addedDict.updateValue(true, forKey: $0) == nil
+        }
+    }
 }
