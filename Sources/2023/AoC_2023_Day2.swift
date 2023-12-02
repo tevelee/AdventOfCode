ec0f1541b73f28f3db49ed432c7bbc74eff6d84e
Laszlo Teveli <tevelee@gmail.com>
2023-12-02 10:24:40 +0100

    2023 Day 2

--- Sources/2023/AoC_2023_Day2.swift
+++ Sources/2023/AoC_2023_Day2.swift
@@ -1,17 +1,67 @@
 import Algorithms
+import RegexBuilder
 
 final class AoC_2023_Day2 {
-    let input: Input
+    fileprivate let games: [Game]
 
-    init(_ input: Input) {
-        self.input = input
+    init(_ input: Input) throws {
+        games = try input.wholeInput.lines.compactMap(Game.init)
     }
 
-    func solvePart1() async throws -> Int {
-        0
+    func solvePart1() -> Int {
+        games.filter { game in
+            game.sessions.allSatisfy {
+                $0.red <= 12 && $0.green <= 13 && $0.blue <= 14
+            }
+        }
+        .sum(of: \.id)
     }
 
-    func solvePart2() async throws -> Int {
-        0
+    func solvePart2() -> Int {
+        games.sum { game in
+            let (red, green, blue) = game.sessions.reduce((red: 0, green: 0, blue: 0)) { result, session in
+                (max(result.red, session.red), max(result.green, session.green), max(result.blue, session.blue))
+            }
+            return red * green * blue
+        }
     }
 }
+
+private struct Game {
+    let id: Int
+    let sessions: [Session]
+
+    struct Session {
+        let red: Int
+        let green: Int
+        let blue: Int
+    }
+
+    init?(raw: String) {
+        guard let match = raw.wholeMatch(of: /Game (?<id>\d+): (?<sessions>.*)/),
+            let id = Int(match.id) else { return nil }
+        self.id = id
+        self.sessions = match.sessions.split(separator: "; ").map { rawSession in
+            Session(
+                red: Self.parseNumber(of: "red", from: rawSession),
+                green: Self.parseNumber(of: "green", from: rawSession),
+                blue: Self.parseNumber(of: "blue", from: rawSession)
+            )
+        }
+    }
+
+    private static func parseNumber(of color: String, from input: Substring, defaultValue: Int = 0) -> Int {
+        input.firstMatch(of: Regex {
+            Capture {
+                OneOrMore {
+                    .digit
+                }
+            } transform: { value in
+                Int(value)
+            }
+            " "
+            color
+        })?.output.1 ?? defaultValue
+    }
+}
+
--- Tests/2023/2023Tests.swift
+++ Tests/2023/2023Tests.swift
@@ -44,4 +44,32 @@ final class AoC_2023_Tests: XCTestCase {
         XCTAssertEqual(result, answer)
         print("Day1 / Part2 solution: \(answer)")
     }
+
+    func test_day2_intro() throws {
+        let problem = try AoC_2023_Day2("""
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """)
+        let part1 = problem.solvePart1()
+        XCTAssertEqual(part1, 8)
+        let part2 = problem.solvePart2()
+        XCTAssertEqual(part2, 2286)
+    }
+
+    func test_day2() throws {
+        let problem = try AoC_2023_Day2(file("2023_day2"))
+
+        var answer = 2551
+        var result = problem.solvePart1()
+        XCTAssertEqual(result, answer)
+        print("Day2 / Part1 solution: \(answer)")
+
+        answer = 62811
+        result = problem.solvePart2()
+        XCTAssertEqual(result, answer)
+        print("Day2 / Part2 solution: \(answer)")
+    }
 }
