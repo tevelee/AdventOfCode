313fad1bae24d5349e91c645d8419240840fd12f
Laszlo Teveli <tevelee@gmail.com>
2023-12-04 11:34:59 +0100

    2023 Day 4

--- Sources/2023/AoC_2023_Day4.swift
+++ Sources/2023/AoC_2023_Day4.swift
@@ -1,17 +1,46 @@
 import Algorithms
 
 final class AoC_2023_Day4 {
-    let input: Input
+    private let cards: [Card]
 
-    init(_ input: Input) {
-        self.input = input
+    init(_ input: Input) throws {
+        cards = try input.wholeInput.lines.compactMap(Card.init)
     }
 
-    func solvePart1() async throws -> Int {
-        0
+    func solvePart1() -> Int {
+        cards.sum { card in
+            pow(card.numberOfMatches, base: 2) / 2
+        }
     }
 
-    func solvePart2() async throws -> Int {
-        0
+    func solvePart2() -> Int {
+        var instances: [Int: Int] = cards.indices.grouped { $0 }.mapValues { _ in 1 }
+        for (offset, card) in self.cards.enumerated() {
+            let multiplier = instances[offset, default: 1]
+            for additional in 0..<card.numberOfMatches {
+                instances[offset + additional + 1, default: 1] += multiplier
+            }
+        }
+        return instances.sum(of: \.value)
+    }
+}
+
+private struct Card {
+    let id: Int
+    let winnerNumbers: [Int]
+    let allNumbers: [Int]
+
+    init?(rawString: String) {
+        guard let values = rawString.wholeMatch(of: /Card.*?(?<id>\d+): (?<winning>.*?) \| (?<all>.*)/)?.output,
+              let id = Int(values.id) else {
+            return nil
+        }
+        self.id = id
+        self.winnerNumbers = values.winning.split(separator: " ").compactMap { Int($0) }
+        self.allNumbers = values.all.split(separator: " ").compactMap { Int($0) }
+    }
+
+    var numberOfMatches: Int {
+        Set(allNumbers).intersection(Set(winnerNumbers)).count
     }
 }
--- Tests/2023/2023Tests.swift
+++ Tests/2023/2023Tests.swift
@@ -105,4 +105,33 @@ final class AoC_2023_Tests: XCTestCase {
         XCTAssertEqual(result, answer)
         print("Day3 / Part2 solution: \(answer)")
     }
+
+    func test_day4_intro() throws {
+        let problem = try AoC_2023_Day4("""
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        """)
+        let part1 = problem.solvePart1()
+        XCTAssertEqual(part1, 13)
+        let part2 = problem.solvePart2()
+        XCTAssertEqual(part2, 30)
+    }
+
+    func test_day4() throws {
+        let problem = try AoC_2023_Day4(file("2023_day4"))
+
+        var answer = 27059
+        var result = problem.solvePart1()
+        XCTAssertEqual(result, answer)
+        print("Day4 / Part1 solution: \(answer)")
+
+        answer = 5744979
+        result = problem.solvePart2()
+        XCTAssertEqual(result, answer)
+        print("Day4 / Part2 solution: \(answer)")
+    }
 }
