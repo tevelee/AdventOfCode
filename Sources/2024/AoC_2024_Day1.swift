25b6733a946479e4f444b16b5b8a078119df7577
Laszlo Teveli <tevelee@gmail.com>
2024-12-01 14:52:58 +0100

    2024 Day 1

--- Sources/2024/AoC_2024_Day1.swift
+++ Sources/2024/AoC_2024_Day1.swift
@@ -1,18 +1,23 @@
 import Utils
 
 public final class AoC_2024_Day1 {
-    let input: Input
+    private let left: [Int]
+    private let right: [Int]
 
-    public init(_ input: Input) {
-        self.input = input
     public init(_ input: Input) async throws {
+        (left, right) = try await input.lines.reduce(into: (left: [Int](), right: [Int]())) { result, item in
+            let integers = item.integers
+            result.left.append(integers[0])
+            result.right.append(integers[1])
+        }
     }
 
     public func solvePart1() async throws -> Int {
-        0
+        zip(left.sorted(), right.sorted()).sum { abs($0 - $1) }
     }
 
     public func solvePart2() async throws -> Int {
-        0
+        let frequencies = right.grouped(by: \.self).mapValues(\.count)
+        return left.sum { frequencies[$0, default: 0] * $0 }
     }
 }
--- Tests/2024/Day1.swift
+++ Tests/2024/Day1.swift
@@ -8,22 +8,33 @@ private struct Day1 {
     @Test
     func part1_intro() async throws {
         let problem = try await CurrentPuzzle("""
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
         """)
-        try await #expect(problem.solvePart1() == 0)
+        try await #expect(problem.solvePart1() == 11)
     }
 
     @Test
     func part2_intro() async throws {
         let problem = try await CurrentPuzzle("""
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
         """)
-        try await #expect(problem.solvePart2() == 0)
+        try await #expect(problem.solvePart2() == 31)
     }
 
     @Test(.tags(.live))
     func live() async throws {
-        let problem = try CurrentPuzzle()
-        try await #expect(problem.solvePart1() == 0)
         let problem = try await CurrentPuzzle()
-        try await #expect(problem.solvePart2() == 0)
+        try await #expect(problem.solvePart1() == 2_375_403)
+        try await #expect(problem.solvePart2() == 23_082_277)
     }
 }
