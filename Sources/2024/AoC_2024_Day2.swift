1eacaf1e62eb2203465b6f3d3ad045633a7655f9
Laszlo Teveli <tevelee@gmail.com>
2024-12-02 11:04:12 +0100

    2024 Day 2

--- Sources/2024/AoC_2024_Day2.swift
+++ Sources/2024/AoC_2024_Day2.swift
@@ -1,17 +1,44 @@
 import Utils
 
 public final class AoC_2024_Day2 {
-    let input: Input
+    private let reports: AnyAsyncSequence<[Int]>
 
     public init(_ input: Input) {
-        self.input = input
+        reports = input.lines.map(\.integers).eraseToAnyAsyncSequence()
     }
 
     public func solvePart1() async throws -> Int {
-        0
+        try await reports.count(where: Self.isValidReport)
     }
 
     public func solvePart2() async throws -> Int {
-        0
+        try await reports.count { levels in
+            levels.indices.contains { index in
+                Self.isValidReport(levels.removing(index))
+            }
+        }
+    }
+
+    private static func isValidReport(_ levels: [Int]) -> Bool {
+        isValidReport(levels) { $0 - $1 }
+        ||
+        isValidReport(levels) { $1 - $0 }
+
+    }
+
+    private static func isValidReport(_ levels: [Int], differenceAlgorithm: (Int, Int) -> Int) -> Bool {
+        levels.adjacentPairs().map(differenceAlgorithm).allSatisfy(isValidDifference)
+    }
+
+    private static func isValidDifference(_ diff: Int) -> Bool {
+        (1...3).contains(diff)
+    }
+}
+
+private extension RangeReplaceableCollection {
+    func removing(_ index: Index) -> Self {
+        var modified = self
+        modified.remove(at: index)
+        return modified
     }
 }
--- Tests/2024/Day2.swift
+++ Tests/2024/Day2.swift
@@ -8,21 +8,33 @@ private struct Day2 {
     @Test
     func part1_intro() async throws {
         let problem = CurrentPuzzle("""
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
         """)
-        try await #expect(problem.solvePart1() == 0)
+        try await #expect(problem.solvePart1() == 2)
     }
 
     @Test
     func part2_intro() async throws {
         let problem = CurrentPuzzle("""
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
         """)
-        try await #expect(problem.solvePart2() == 0)
+        try await #expect(problem.solvePart2() == 4)
     }
 
     @Test(.tags(.live))
     func live() async throws {
         let problem = try await CurrentPuzzle()
-        try await #expect(problem.solvePart1() == 0)
-        try await #expect(problem.solvePart2() == 0)
+        try await #expect(problem.solvePart1() == 220)
+        try await #expect(problem.solvePart2() == 296)
     }
 }
