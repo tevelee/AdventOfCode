2c4e0cf86756ae664e97b04c2903f1b492145272
Laszlo Teveli <tevelee@gmail.com>
2024-12-03 11:11:45 +0100

    regex builder

--- Sources/2024/AoC_2024_Day3.swift
+++ Sources/2024/AoC_2024_Day3.swift
@@ -1,33 +1,53 @@
 import Utils
+import RegexBuilder
 
 public final class AoC_2024_Day3 {
     private let instructions: String
+
+    private lazy var number = TryCapture {
+        Repeat(1...3) {
+            One(.digit)
+        }
+    } transform: {
+        Int($0)
+    }
+
+    private lazy var mul = Regex {
+        "mul("
+        number
+        ","
+        number
+        ")"
+    }
 
     public init(_ input: Input) throws {
         instructions = try input.wholeInput
     }
 
     public func solvePart1() async throws -> Int {
-        instructions.matches(of: /mul\((?<lhs>\d{1,3}),(?<rhs>\d{1,3})\)/).sum { match in
-            guard let lhs = Int(match.lhs), let rhs = Int(match.rhs) else {
-                return 0
-            }
-            return lhs * rhs
+        instructions.matches(of: mul).sum { match in
+            match.1 * match.2
         }
     }
 
     public func solvePart2() async throws -> Int {
+        let full = Regex {
+            ChoiceOf {
+                "do()"
+                "don't()"
+                mul
+            }
+        }
+
         var enabled = true
-        return instructions.matches(of: /mul\((?<lhs>\d{1,3}),(?<rhs>\d{1,3})\)|do\(\)|don't\(\)/).sum { match in
+        return instructions.matches(of: full).sum { match in
             switch match.output {
                 case ("do()", _, _):
                     enabled = true
                 case ("don't()", _, _):
                     enabled = false
                 case (_, let lhs?, let rhs?) where enabled:
-                    if let lhs = Int(lhs), let rhs = Int(rhs) {
-                        return lhs * rhs
-                    }
+                    return lhs * rhs
                 default:
                     break
             }
