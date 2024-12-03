7bfd72559172df539c3bf64988c69d17b8fdd04b
Laszlo Teveli <tevelee@gmail.com>
2024-12-03 10:50:10 +0100

    2024 Day 3

--- Sources/2024/AoC_2024_Day3.swift
+++ Sources/2024/AoC_2024_Day3.swift
@@ -1,17 +1,37 @@
 import Utils
 
 public final class AoC_2024_Day3 {
-    let input: Input
+    private let instructions: String
 
-    public init(_ input: Input) {
-        self.input = input
+    public init(_ input: Input) throws {
+        instructions = try input.wholeInput
     }
 
     public func solvePart1() async throws -> Int {
-        0
+        instructions.matches(of: /mul\((?<lhs>\d{1,3}),(?<rhs>\d{1,3})\)/).sum { match in
+            guard let lhs = Int(match.lhs), let rhs = Int(match.rhs) else {
+                return 0
+            }
+            return lhs * rhs
+        }
     }
 
     public func solvePart2() async throws -> Int {
-        0
+        var enabled = true
+        return instructions.matches(of: /mul\((?<lhs>\d{1,3}),(?<rhs>\d{1,3})\)|do\(\)|don't\(\)/).sum { match in
+            switch match.output {
+                case ("do()", _, _):
+                    enabled = true
+                case ("don't()", _, _):
+                    enabled = false
+                case (_, let lhs?, let rhs?) where enabled:
+                    if let lhs = Int(lhs), let rhs = Int(rhs) {
+                        return lhs * rhs
+                    }
+                default:
+                    break
+            }
+            return 0
+        }
     }
 }
--- Tests/2024/Day3.swift
+++ Tests/2024/Day3.swift
@@ -7,22 +7,24 @@ extension CurrentPuzzle: Puzzle {}
 private struct Day3 {
     @Test
     func part1_intro() async throws {
-        let problem = CurrentPuzzle("""
+        let problem = try CurrentPuzzle("""
+        xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
         """)
-        try await #expect(problem.solvePart1() == 0)
+        try await #expect(problem.solvePart1() == 161)
     }
 
     @Test
     func part2_intro() async throws {
-        let problem = CurrentPuzzle("""
+        let problem = try CurrentPuzzle("""
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
         """)
-        try await #expect(problem.solvePart2() == 0)
+        try await #expect(problem.solvePart2() == 48)
     }
 
     @Test(.tags(.live))
     func live() async throws {
         let problem = try await CurrentPuzzle()
-        try await #expect(problem.solvePart1() == 0)
-        try await #expect(problem.solvePart2() == 0)
+        try await #expect(problem.solvePart1() == 170_778_545)
+        try await #expect(problem.solvePart2() == 82_868_252)
     }
 }
