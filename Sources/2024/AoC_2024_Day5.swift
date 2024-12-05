fc64c5ed9481c6af1e827d1f2ce159b4f5ecfd02
Laszlo Teveli <tevelee@gmail.com>
2024-12-05 12:39:58 +0100

    2024 Day 5

--- Sources/2024/AoC_2024_Day5.swift
+++ Sources/2024/AoC_2024_Day5.swift
@@ -1,17 +1,55 @@
+import Algorithms
 import Utils
 
 public final class AoC_2024_Day5 {
-    let input: Input
+    private let orderingRules: OrderingRules
+    private let updates: [[Int]]
 
-    public init(_ input: Input) {
-        self.input = input
+    public init(_ input: Input) throws {
+        let (rawRules, rawUpdates) = try input.wholeInput.paragraphs.elements()
+        orderingRules = try OrderingRules(rules: rawRules.map { try $0.integers.elements() })
+        updates = rawUpdates.map(\.integers)
+    }
+
+    private lazy var sortedUpdates = updates.partitioned { update in
+        update.sortingPairs().allSatisfy(orderingRules.isInCorrectOrder)
     }
 
     public func solvePart1() async throws -> Int {
-        0
+        sortedUpdates.trueElements
+            .sum(of: \.middleElement)
     }
 
     public func solvePart2() async throws -> Int {
-        0
+        sortedUpdates.falseElements
+            .map { $0.sorted(by: orderingRules.isInCorrectOrder) }
+            .sum(of: \.middleElement)
+    }
+}
+
+private struct OrderingRules {
+    let precedences: [Int: Set<Int>]
+
+    init(rules: [(before: Int, after: Int)]) {
+        precedences = rules.grouped(by: \.before).mapValues { Set($0.map(\.after)) }
+    }
+
+    func isInCorrectOrder(_ first: Int, _ second: Int) -> Bool {
+        guard let precedenceRule = precedences[second] else { return true }
+        return !precedenceRule.contains(first)
+    }
+}
+
+private extension Collection where Index == Int {
+    var middleElement: Element {
+        self[(endIndex - startIndex) / 2]
+    }
+
+    func sortingPairs() -> some Sequence<(Element, Element)> {
+        indices.dropLast().flatMap { first in
+            indices.dropFirst(first + 1).map { second in
+                (self[first], self[second])
+            }
+        }
     }
 }
--- Tests/2024/Day5.swift
+++ Tests/2024/Day5.swift
@@ -7,22 +7,78 @@ extension CurrentPuzzle: Puzzle {}
 private struct Day5 {
     @Test
     func part1_intro() async throws {
-        let problem = CurrentPuzzle("""
+        let problem = try CurrentPuzzle("""
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+        
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
         """)
-        try await #expect(problem.solvePart1() == 0)
+        try await #expect(problem.solvePart1() == 143)
     }
 
     @Test
     func part2_intro() async throws {
-        let problem = CurrentPuzzle("""
+        let problem = try CurrentPuzzle("""
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+        
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
         """)
-        try await #expect(problem.solvePart2() == 0)
+        try await #expect(problem.solvePart2() == 123)
     }
 
     @Test(.tags(.live))
     func live() async throws {
         let problem = try await CurrentPuzzle()
-        try await #expect(problem.solvePart1() == 0)
-        try await #expect(problem.solvePart2() == 0)
+        try await #expect(problem.solvePart1() == 5_991)
+        try await #expect(problem.solvePart2() == 5_479)
     }
 }
