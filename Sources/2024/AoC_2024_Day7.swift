20a41a51de636de8175ec914f1843b415871621f
Laszlo Teveli <tevelee@gmail.com>
2024-12-07 08:49:59 +0100

    2024 Day 7

--- Sources/2024/AoC_2024_Day7.swift
+++ Sources/2024/AoC_2024_Day7.swift
@@ -1,17 +1,43 @@
 import Utils
 
 public final class AoC_2024_Day7 {
-    let input: Input
+    private let entries: [(target: Int, components: [Int])]
 
-    public init(_ input: Input) {
-        self.input = input
+    public init(_ input: Input) throws {
+        entries = try input.wholeInput.lines.map { line in
+            guard let (target, others) = line.integers.headAndTail else { throw ParseError() }
+            return (target, Array(others))
+        }
     }
 
-    public func solvePart1() async throws -> Int {
-        0
+    public func solvePart1() -> Int {
+        solve(operators: [.multiply, .add])
     }
 
-    public func solvePart2() async throws -> Int {
-        0
+    public func solvePart2() -> Int {
+        solve(operators: [.multiply, .add, .append])
     }
+
+    private func solve(operators: [Operator]) -> Int {
+        entries.sum { entry in
+            canSolve(target: entry.target, components: entry.components, operators: operators) ? entry.target : 0
+        }
+    }
+
+    private func canSolve(target: Int, components: [Int], operators: [Operator]) -> Bool {
+        guard let (first, components) = components.headAndTail, first <= target else { return false }
+        guard let (second, components) = components.headAndTail else { return target == first }
+        for op in operators where canSolve(target: target, components: [op.perform(first, second)] + components, operators: operators) {
+            return true
+        }
+        return false
+    }
+}
+
+private struct Operator {
+    let perform: @Sendable (Int, Int) -> Int
+
+    static let add = Operator { $0 + $1 }
+    static let multiply = Operator { $0 * $1 }
+    static let append = Operator { Int("\($0)\($1)")! }
 }
--- Tests/2024/Day7.swift
+++ Tests/2024/Day7.swift
@@ -7,22 +7,40 @@ extension CurrentPuzzle: Puzzle {}
 private struct Day7 {
     @Test
     func part1_intro() async throws {
-        let problem = CurrentPuzzle("""
+        let problem = try CurrentPuzzle("""
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
         """)
-        try await #expect(problem.solvePart1() == 0)
+        #expect(problem.solvePart1() == 3_749)
     }
 
     @Test
     func part2_intro() async throws {
-        let problem = CurrentPuzzle("""
+        let problem = try CurrentPuzzle("""
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
         """)
-        try await #expect(problem.solvePart2() == 0)
+        #expect(problem.solvePart2() == 11_387)
     }
 
     @Test(.tags(.live))
     func live() async throws {
         let problem = try await CurrentPuzzle()
-        try await #expect(problem.solvePart1() == 0)
-        try await #expect(problem.solvePart2() == 0)
+        #expect(problem.solvePart1() == 5_512_534_574_980)
+        #expect(problem.solvePart2() == 328_790_210_468_594)
     }
 }
