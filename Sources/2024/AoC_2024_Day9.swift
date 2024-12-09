daf35bf2455c0a2d71ef309a9ff1350494d6bbd1
Laszlo Teveli <tevelee@gmail.com>
2024-12-09 13:04:42 +0100

    2024 Day 9

--- Sources/2024/AoC_2024_Day9.swift
+++ Sources/2024/AoC_2024_Day9.swift
@@ -1,17 +1,113 @@
 import Utils
 
 public final class AoC_2024_Day9 {
-    let input: Input
+    let disk: [DiskElement]
 
-    public init(_ input: Input) {
-        self.input = input
+    public init(_ input: Input) throws {
+        let diskMap = try input.wholeInput
+        var disk: [DiskElement] = []
+        var id = 0
+        for (index, value) in diskMap.compactMap(\.wholeNumberValue).enumerated() {
+            let isFileBlock = index.isMultiple(of: 2)
+            disk.append(DiskElement(count: value, type: isFileBlock ? .fileBlock(id: id) : .emptySpace))
+            if isFileBlock { id += 1 }
+        }
+        self.disk = disk
     }
 
-    public func solvePart1() async throws -> Int {
-        0
+    public func solvePart1() -> Int {
+        var disk = disk
+        while var emptyIndex = disk.firstIndex(where: \.isEmpty) {
+            while disk[emptyIndex].count > 0, let fileIndex = disk.lastIndex(where: \.isFile) {
+                let size = min(disk[emptyIndex].count, disk[fileIndex].count)
+                disk[fileIndex].count -= size
+                disk[emptyIndex].count -= size
+                disk.insert(DiskElement(count: size, type: disk[fileIndex].type), at: emptyIndex)
+                emptyIndex += 1
+            }
+        }
+        return checksum(of: disk)
     }
 
-    public func solvePart2() async throws -> Int {
-        0
+    public func solvePart2() -> Int {
+        var disk = disk
+        var processed: Set<Int> = []
+        files: while let fileIndex = disk.lastIndex(where: { $0.isFile && !processed.contains($0.type.id!) }) {
+            let file = disk[fileIndex]
+            if let emptyIndex = disk.firstIndex(where: { $0.isEmpty && $0.count >= disk[fileIndex].count }), emptyIndex < fileIndex {
+                let size = file.count
+                disk[fileIndex] = DiskElement(count: size, type: .emptySpace)
+                disk[emptyIndex].count -= size
+                disk.insert(DiskElement(count: size, type: file.type), at: emptyIndex)
+            }
+            processed.insert(file.type.id!)
+        }
+        return checksum(of: disk)
+    }
+
+    private func checksum(of disk: [DiskElement]) -> Int {
+        var checksum = 0
+        var offset = 0
+        for element in disk {
+            switch element.type {
+                case .fileBlock(let id):
+                    for _ in 0 ..< element.count {
+                        checksum += id * offset
+                        offset += 1
+                    }
+                case .emptySpace:
+                    offset += element.count
+            }
+        }
+        return checksum
+    }
+}
+
+struct DiskElement: CustomStringConvertible {
+    var count: Int
+    let type: DiskElementType
+
+    enum DiskElementType: CustomStringConvertible {
+        case emptySpace
+        case fileBlock(id: Int)
+
+        var isEmpty: Bool {
+            switch self {
+                case .emptySpace: true
+                case .fileBlock: false
+            }
+        }
+
+        var id: Int? {
+            switch self {
+                case .emptySpace: nil
+                case .fileBlock(let id): id
+            }
+        }
+
+        var description: String {
+            switch self {
+                case .emptySpace: "."
+                case .fileBlock(let id): String(id)
+            }
+        }
+    }
+
+    var isEmpty: Bool {
+        type.isEmpty && count > 0
+    }
+
+    var isFile: Bool {
+        !type.isEmpty && count > 0
+    }
+
+    var description: String {
+        String(repeating: type.description, count: count)
+    }
+}
+
+extension [DiskElement] {
+    var description: String {
+        map(\.description).joined(separator: "")
     }
 }
--- Tests/2024/Day9.swift
+++ Tests/2024/Day9.swift
@@ -6,23 +6,21 @@ extension CurrentPuzzle: Puzzle {}
 
 private struct Day9 {
     @Test
-    func part1_intro() async throws {
-        let problem = CurrentPuzzle("""
-        """)
-        try await #expect(problem.solvePart1() == 0)
+    func part1_intro() throws {
+        let problem = try CurrentPuzzle("2333133121414131402")
+        #expect(problem.solvePart1() == 1_928)
     }
 
     @Test
-    func part2_intro() async throws {
-        let problem = CurrentPuzzle("""
-        """)
-        try await #expect(problem.solvePart2() == 0)
+    func part2_intro() throws {
+        let problem = try CurrentPuzzle("2333133121414131402")
+        #expect(problem.solvePart2() == 2_858)
     }
 
     @Test(.tags(.live))
     func live() async throws {
         let problem = try await CurrentPuzzle()
-        try await #expect(problem.solvePart1() == 0)
-        try await #expect(problem.solvePart2() == 0)
+        #expect(problem.solvePart1() == 6_154_342_787_400)
+        #expect(problem.solvePart2() == 6_183_632_723_350)
     }
 }
