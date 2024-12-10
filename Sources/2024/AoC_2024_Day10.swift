2285286427a97e5834fc4a772e8dd5d7b99961b9
Laszlo Teveli <tevelee@gmail.com>
2024-12-10 12:44:45 +0100

    2024 Day 10

--- Sources/2024/AoC_2024_Day10.swift
+++ Sources/2024/AoC_2024_Day10.swift
@@ -1,17 +1,87 @@
 import Utils
 
 public final class AoC_2024_Day10 {
-    let input: Input
+    private let grid: [[Int]]
+    private let positions: [Int: Set<Position>]
 
-    public init(_ input: Input) {
-        self.input = input
+    public init(_ input: Input) throws {
+        let grid: [[Int]] = try input.wholeInput.lines.map { Array($0).map { $0.wholeNumberValue ?? -1 } }
+        var positions = grid.positions.grouped { grid[$0] }.mapValues { Set($0) }
+        positions[-1] = nil
+        self.grid = grid
+        self.positions = positions
     }
 
-    public func solvePart1() async throws -> Int {
-        0
+    public func solvePart1() throws -> Int {
+        var reachableTrailheads: [[Set<Position>]] = grid.map { $0.map { _ in [] } }
+        for position in positions[9, default: []] {
+            reachableTrailheads[position] = [position]
+        }
+        forEachTrail { position, trails in
+            reachableTrailheads[position] = Set(trails.flatMap { reachableTrailheads[$0] })
+        }
+        return positions[0, default: []].sum { reachableTrailheads[$0].count }
     }
 
-    public func solvePart2() async throws -> Int {
-        0
+    public func solvePart2() throws -> Int {
+        var numberOfDistinctTrails: [[Int]] = grid.map { $0.map { _ in 0 } }
+        for position in positions[9, default: []] {
+            numberOfDistinctTrails[position] = 1
+        }
+        forEachTrail { position, trails in
+            numberOfDistinctTrails[position] = trails.sum { numberOfDistinctTrails[$0] }
+        }
+        return positions[0, default: []].sum { numberOfDistinctTrails[$0] }
+    }
+
+    private func forEachTrail(_ block: (Position, [Position]) -> Void) {
+        for (height, positions) in positions.sorted(by: \.key, comparator: >).dropFirst() {
+            for position in positions {
+                block(position, grid.neighbors(of: position).filter { grid[$0] == height + 1 })
+            }
+        }
+    }
+}
+
+private struct Position: Hashable, CustomStringConvertible {
+    var x, y: Int
+
+    func apply(_ block: (inout Self) -> Void) -> Self {
+        var copy = self
+        block(&copy)
+        return copy
+    }
+
+    var description: String { "(\(x),\(y))" }
+}
+
+private extension Array where Element: RandomAccessCollection, Element.Index == Int {
+    var positions: [Position] {
+        enumerated().flatMap { row, line in
+            line.indices.map { Position(x: $0, y: row) }
+        }
+    }
+
+    func neighbors(of position: Position) -> [Position] {
+        [
+            position.apply { $0.x += 1 },
+            position.apply { $0.x -= 1 },
+            position.apply { $0.y += 1 },
+            position.apply { $0.y -= 1 }
+        ].filter(contains)
+    }
+
+    private var width: Int { first?.endIndex ?? 0 }
+    private var height: Int { endIndex }
+
+    private func contains(position: Position) -> Bool {
+        (0 ..< width).contains(position.x) && (0 ..< height).contains(position.y)
+    }
+}
+
+private extension Array where Element: RandomAccessCollection & MutableCollection, Element.Index == Int {
+    subscript(position: Position) -> Element.Element {
+        get { self[position.y][position.x] }
+        set { self[position.y][position.x] = newValue }
     }
 }
--- Tests/2024/Day10.swift
+++ Tests/2024/Day10.swift
@@ -6,23 +6,92 @@ extension CurrentPuzzle: Puzzle {}
 
 private struct Day10 {
     @Test
-    func part1_intro() async throws {
-        let problem = CurrentPuzzle("""
+    func part1_intro1() throws {
+        let problem = try CurrentPuzzle("""
+        0123
+        1234
+        8765
+        9876
         """)
-        try await #expect(problem.solvePart1() == 0)
+        try #expect(problem.solvePart1() == 1)
     }
 
     @Test
-    func part2_intro() async throws {
-        let problem = CurrentPuzzle("""
+    func part1_intro2() throws {
+        let problem = try CurrentPuzzle("""
+        ...0...
+        ...1...
+        ...2...
+        6543456
+        7.....7
+        8.....8
+        9.....9
         """)
-        try await #expect(problem.solvePart2() == 0)
+        try #expect(problem.solvePart1() == 2)
+    }
+
+    @Test
+    func part1_intro3() throws {
+        let problem = try CurrentPuzzle("""
+        ..90..9
+        ...1.98
+        ...2..7
+        6543456
+        765.987
+        876....
+        987....
+        """)
+        try #expect(problem.solvePart1() == 4)
+    }
+
+    @Test
+    func part1_intro4() throws {
+        let problem = try CurrentPuzzle("""
+        10..9..
+        2...8..
+        3...7..
+        4567654
+        ...8..3
+        ...9..2
+        .....01
+        """)
+        try #expect(problem.solvePart1() == 3)
+    }
+
+    @Test
+    func part1_intro5() throws {
+        let problem = try CurrentPuzzle("""
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
+        """)
+        try #expect(problem.solvePart1() == 36)
+    }
+
+    @Test
+    func part2_intro() throws {
+        let problem = try CurrentPuzzle("""
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
+        """)
+        try #expect(problem.solvePart2() == 81)
     }
 
     @Test(.tags(.live))
     func live() async throws {
         let problem = try await CurrentPuzzle()
-        try await #expect(problem.solvePart1() == 0)
-        try await #expect(problem.solvePart2() == 0)
+        try #expect(problem.solvePart1() == 717)
+        try #expect(problem.solvePart2() == 1_686)
     }
 }
