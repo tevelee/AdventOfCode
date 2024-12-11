bc6321fc81204273545f5399fa7bd502046ca9ab
Laszlo Teveli <tevelee@gmail.com>
2024-12-11 13:12:24 +0100

    Memoization

--- Sources/2024/AoC_2024_Day11.swift
+++ Sources/2024/AoC_2024_Day11.swift
@@ -1,40 +1,43 @@
 import Utils
 
 public final class AoC_2024_Day11 {
-    private let stones: [Stone]
-    private var cache: [Stone: [Iteration: Int]] = [:]
-
-    private typealias Stone = Int
-    private typealias Iteration = Int
+    private let stones: [Int]
 
     public init(_ input: Input) throws {
         stones = try input.wholeInput.integers
     }
 
-    public func solve(times interation: Int) -> Int {
-        stones.sum {
-            iterate(stone: $0, times: interation)
-        }
+    private struct Blink: Hashable {
+        let stone: Int
+        let iteration: Int
     }
-    
-    private func iterate(stone: Int, times iteration: Int) -> Int {
-        cached(stone: stone, iteration: iteration) {
-            computeNumberOfStones(from: stone, at: iteration)
+
+    public func solve(times iteration: Int) -> Int {
+        computeNumberOfStones(for: stones, after: iteration)
+    }
+
+    private func computeNumberOfStones(for stones: [Int], after iteration: Int) -> Int {
+        stones.sum { stone in
+            computeNumberOfStones(Blink(stone: stone, iteration: iteration))
         }
     }
 
-    private func computeNumberOfStones(from stone: Int, at iteration: Int) -> Int {
-        if iteration == 0 {
+    private lazy var computeNumberOfStones = memoize(_computeNumberOfStones)
+    private func _computeNumberOfStones(at blink: Blink) -> Int {
+        if blink.iteration == 0 {
             1
         } else {
-            next(for: stone).sum {
-                iterate(stone: $0, times: iteration - 1)
-            }
+            computeNumberOfStones(
+                for: newStones(from: blink.stone),
+                after: blink.iteration - 1
+            )
         }
     }
 
-    private func next(for stone: Int) -> [Int] {
-        guard stone > 0 else { return [1] }
+    private func newStones(from stone: Int) -> [Int] {
+        if stone == 0 {
+            return [1]
+        }
         let string = String(stone)
         let numberOfDigits = string.utf8.count
         let (quotient, remainder) = numberOfDigits.quotientAndRemainder(dividingBy: 2)
@@ -45,14 +48,5 @@ public final class AoC_2024_Day11 {
         } else {
             [stone * 2024]
         }
-    }
-
-    private func cached(stone: Int, iteration: Int, computation: () -> Int) -> Int {
-        if let cached = cache[stone]?[iteration] {
-            return cached
-        }
-        let count = computation()
-        cache[stone, default: [:]][iteration] = count
-        return count
     }
 }
