b852904ab5f2790279c1758f919857d0afa83ef5
Laszlo Teveli <tevelee@gmail.com>
2024-12-19 14:00:28 +0100

    Simplification

--- Sources/2024/AoC_2024_Day19.swift
+++ Sources/2024/AoC_2024_Day19.swift
@@ -20,20 +20,26 @@ public final class AoC_2024_Day19 {
 
     private lazy var canPrint = memoize(_canPrint)
     private func _canPrint(design: ArraySlice<Color>) -> Bool {
-        if design.isEmpty { return true }
-        return patterns.lazy
-            .filter(design.hasPrefix)
-            .map { design.dropFirst($0.count) }
-            .contains(where: canPrint)
+        if design.isEmpty {
+            true
+        } else {
+            prefixingPatterns(from: design).contains(where: canPrint)
+        }
     }
 
     private lazy var numberOfArrangements = memoize(_numberOfArrangements)
     private func _numberOfArrangements(of design: ArraySlice<Color>) -> Int {
-        if design.isEmpty { return 1 }
-        return patterns.lazy
+        if design.isEmpty {
+            1
+        } else {
+            prefixingPatterns(from: design).sum(of: numberOfArrangements)
+        }
+    }
+
+    private func prefixingPatterns(from design: ArraySlice<Color>) -> some Sequence<ArraySlice<Color>> {
+        patterns.lazy
             .filter(design.hasPrefix)
             .map { design.dropFirst($0.count) }
-            .sum(of: numberOfArrangements)
     }
 }
 
