feb846c08fcdcd003d4147fb626a489a70a22e6c
Laszlo Teveli <tevelee@gmail.com>
2024-12-19 13:55:07 +0100

    Single recursion

--- Sources/2024/AoC_2024_Day19.swift
+++ Sources/2024/AoC_2024_Day19.swift
@@ -20,20 +20,23 @@ public final class AoC_2024_Day19 {
 
     private lazy var canPrint = memoize(_canPrint)
     private func _canPrint(design: ArraySlice<Color>) -> Bool {
-        if design.isEmpty { return true }
-        return patterns.lazy
-            .filter(design.hasPrefix)
-            .map { design.dropFirst($0.count) }
-            .contains(where: canPrint)
+        recurse(design) { $0?.contains(where: canPrint) ?? true }
     }
 
     private lazy var numberOfArrangements = memoize(_numberOfArrangements)
-    private func _numberOfArrangements(of design: ArraySlice<Color>) -> Int {
-        if design.isEmpty { return 1 }
-        return patterns.lazy
-            .filter(design.hasPrefix)
-            .map { design.dropFirst($0.count) }
-            .sum(of: numberOfArrangements)
+    private func _numberOfArrangements(design: ArraySlice<Color>) -> Int {
+        recurse(design) { $0?.sum(of: numberOfArrangements) ?? 1 }
+    }
+
+    private func recurse<Result>(
+        _ design: ArraySlice<Color>,
+        aggregate: (LazyMapSequence<LazyFilterSequence<Array<Array<Color>>>, ArraySlice<Color>>?) -> Result // some Sequence<ArraySlice<Color>>?
+    ) -> Result {
+        aggregate(
+            design.isEmpty ? nil : patterns.lazy
+                .filter(design.hasPrefix)
+                .map { design.dropFirst($0.count) }
+        )
     }
 }
 
