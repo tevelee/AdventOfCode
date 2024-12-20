88202372dbe7c4f3e51b7a4a7daec7c987802274
Laszlo Teveli <tevelee@gmail.com>
2024-12-20 16:25:37 +0100

    2024 Day 20

--- Sources/2024/AoC_2024_Day20.swift
+++ Sources/2024/AoC_2024_Day20.swift
@@ -1,17 +1,107 @@
+import Algorithms
+import Graphs
 import Utils
 
 public final class AoC_2024_Day20 {
-    let input: Input
+    private let walls: Set<Position>
+    private let start: Position
+    private let end: Position
 
-    public init(_ input: Input) {
-        self.input = input
+    public init(_ input: Input) throws {
+        var walls: Set<Position> = []
+        var start: Position?
+        var end: Position?
+        for (row, line) in try input.wholeInput.lines.enumerated() {
+            for (column, character) in line.enumerated() {
+                let position = Position(x: column, y: row)
+                switch character {
+                case "#": walls.insert(position)
+                case "S": start = position
+                case "E": end = position
+                default: break
+                }
+            }
+        }
+        self.walls = walls
+        guard let start, let end else { throw ParseError() }
+        self.start = start
+        self.end = end
     }
 
-    public func solvePart1() async throws -> Int {
-        0
+    public func solvePart1() -> Int {
+        solve(max: 2) { $0 >= 100 }
     }
 
-    public func solvePart2() async throws -> Int {
-        0
+    public func solvePart2() -> Int {
+        solve(max: 20) { $0 >= 100 }
+    }
+
+    public func solve(max: Int, saves: Int) -> Int {
+        solve(max: max) { $0 == saves }
+    }
+
+    private lazy var shortestPath = LazyGraph { [walls] in $0.neighbors.filter { !walls.contains($0) } }
+        .weighted(constant: 1 as UInt)
+        .shortestPath(from: start, to: end, using: .dijkstra())!.path
+
+    private lazy var offsets = shortestPath.enumerated().keyed(by: \.element).mapValues(\.offset)
+
+    private func solve(max maxLength: Int, saveCondition: @escaping (Int) -> Bool) -> Int {
+        shortestPath
+            .combinations(ofCount: 2)
+            .map { try! $0.elements() }
+            .count { [walls] p1, p2 in
+                let length = abs(p1.x - p2.x) + abs(p1.y - p2.y) - 1
+                guard length > 0, length < maxLength else { return false }
+                let saving = abs(offsets[p1]! - offsets[p2]!) - length - 1
+                guard saveCondition(saving) else { return false }
+                return LazyGraph { $0.neighbors.filter { walls.contains($0) || $0 == p2 } }
+                    .weighted(constant: 1 as UInt)
+                    .shortestPath(from: p1, to: p2, using: .aStar(heuristic: .manhattanDistance(of: \.coordinate))) != nil
+            }
+    }
+
+    private func visualize(shortcut: [Position] = []) {
+        let x = walls.minAndMaxValues(of: \.x)!
+        let y = walls.minAndMaxValues(of: \.y)!
+        for y in y.min ... y.max {
+            for x in x.min ... x.max {
+                let position = Position(x: x, y: y)
+                let character: Character = if shortcut.contains(position) {
+                    "X"
+                } else if walls.contains(position) {
+                    "#"
+                } else if position == start {
+                    "S"
+                } else if position == end {
+                    "E"
+                } else {
+                    "."
+                }
+                print(character, terminator: "")
+            }
+            print()
+        }
+    }
+}
+
+private struct Position: Hashable, CustomStringConvertible {
+    let x, y: Int
+
+    var neighbors: [Position] {
+        [
+            Position(x: x, y: y - 1),
+            Position(x: x + 1, y: y),
+            Position(x: x, y: y + 1),
+            Position(x: x - 1, y: y)
+        ]
+    }
+
+    var coordinate: SIMD2<Double> {
+        .init(x: Double(x), y: Double(y))
+    }
+
+    var description: String {
+        "(\(x),\(y))"
     }
 }
--- Tests/2024/Day20.swift
+++ Tests/2024/Day20.swift
@@ -6,34 +6,87 @@ extension CurrentPuzzle: Puzzle {}
 
 private struct Day20 {
     @Test
-    func part1_intro() async throws {
-        let problem = CurrentPuzzle("""
+    func part1_intro() throws {
+        let problem = try CurrentPuzzle("""
+        ###############
+        #...#...#.....#
+        #.#.#.#.#.###.#
+        #S#...#.#.#...#
+        #######.#.#.###
+        #######.#.#...#
+        #######.#.###.#
+        ###..E#...#...#
+        ###.#######.###
+        #...###...#...#
+        #.#####.#.###.#
+        #.#...#.#.#...#
+        #.#.#.#.#.#.###
+        #...#...#...###
+        ###############
         """)
-        try await #expect(problem.solvePart1() == 0)
+        #expect(problem.solve(max: 2, saves: 2) == 14)
+        #expect(problem.solve(max: 2, saves: 4) == 14)
+        #expect(problem.solve(max: 2, saves: 6) == 2)
+        #expect(problem.solve(max: 2, saves: 8) == 4)
+        #expect(problem.solve(max: 2, saves: 10) == 2)
+        #expect(problem.solve(max: 2, saves: 12) == 3)
+        #expect(problem.solve(max: 2, saves: 20) == 1)
+        #expect(problem.solve(max: 2, saves: 36) == 1)
+        #expect(problem.solve(max: 2, saves: 38) == 1)
+        #expect(problem.solve(max: 2, saves: 40) == 1)
+        #expect(problem.solve(max: 2, saves: 64) == 1)
     }
 
     @Test
-    func part2_intro() async throws {
-        let problem = CurrentPuzzle("""
+    func part2_intro() throws {
+        let problem = try CurrentPuzzle("""
+        ###############
+        #...#...#.....#
+        #.#.#.#.#.###.#
+        #S#...#.#.#...#
+        #######.#.#.###
+        #######.#.#...#
+        #######.#.###.#
+        ###..E#...#...#
+        ###.#######.###
+        #...###...#...#
+        #.#####.#.###.#
+        #.#...#.#.#...#
+        #.#.#.#.#.#.###
+        #...#...#...###
+        ###############
         """)
-        try await #expect(problem.solvePart2() == 0)
+        #expect(problem.solve(max: 20, saves: 50) == 32)
+        #expect(problem.solve(max: 20, saves: 52) == 31)
+        #expect(problem.solve(max: 20, saves: 54) == 29)
+        #expect(problem.solve(max: 20, saves: 56) == 39)
+        #expect(problem.solve(max: 20, saves: 58) == 25)
+        #expect(problem.solve(max: 20, saves: 60) == 23)
+        #expect(problem.solve(max: 20, saves: 62) == 20)
+        #expect(problem.solve(max: 20, saves: 64) == 19)
+        #expect(problem.solve(max: 20, saves: 66) == 12)
+        #expect(problem.solve(max: 20, saves: 68) == 14)
+        #expect(problem.solve(max: 20, saves: 70) == 12)
+        #expect(problem.solve(max: 20, saves: 72) == 22)
+        #expect(problem.solve(max: 20, saves: 74) == 4)
+        #expect(problem.solve(max: 20, saves: 76) == 3)
     }
 
-    @Suite(.tags(.live), .serialized)
-    struct Day20Live {
+    @Suite(.tags(.live), .serialized, .disabled("runs too long"))
+    struct Day20Live: @unchecked Sendable {
         private let problem: CurrentPuzzle
         init() async throws {
             problem = try await CurrentPuzzle()
         }
-        
+
         @Test("Day 20 Part 1")
-        func part1() async throws {
-            try await #expect(problem.solvePart1() == 0)
+        func part1() {
+            #expect(problem.solvePart1() == 1_429)
         }
-        
+
         @Test("Day 20 Part 2")
-        func part2() async throws {
-            try await #expect(problem.solvePart2() == 0)
+        func part2() {
+            #expect(problem.solvePart2() == 988_931)
         }
     }
 }
