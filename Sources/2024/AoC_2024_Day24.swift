6af8748bc9e6e714716cdd0abc988b9f247b3bb5
Laszlo Teveli <tevelee@gmail.com>
2024-12-25 16:51:11 +0100

    2024 Day 24

--- Sources/2024/AoC_2024_Day24.swift
+++ Sources/2024/AoC_2024_Day24.swift
@@ -1,17 +1,158 @@
 import Utils
 
 public final class AoC_2024_Day24 {
-    let input: Input
+    private let registers: [String: Value]
+    private let instructions: [Instruction]
 
-    public init(_ input: Input) {
-        self.input = input
+    public init(_ input: Input) throws {
+        let (registers, instructions) = try input.wholeInput.paragraphs.elements()
+        self.instructions = instructions.compactMap { line in
+            guard let match = line.wholeMatch(of: /(?<lhs>.*) (?<gate>.*) (?<rhs>.*) -> (?<result>.*)/),
+                  let gate = Gate(rawValue: String(match.gate).lowercased()) else {
+                return nil
+            }
+            return Instruction(gate: gate, lhs: String(match.lhs), rhs: String(match.rhs), output: String(match.result))
+        }
+        self.registers = registers.reduce(into: [:]) { output, line in
+            guard let match = line.wholeMatch(of: /(?<name>.*): (?<value>[01])/) else { return }
+            output[String(match.name)] = .set(match.value == "1")
+        } + self.instructions.reduce(into: [:]) { $0[$1.output] = .unset }
     }
 
-    public func solvePart1() async throws -> Int {
-        0
+    private lazy var xRegisters = registers(prefixed: "x")
+    private lazy var yRegisters = registers(prefixed: "y")
+    private lazy var zRegisters = registers(prefixed: "z")
+
+    public func solvePart1() -> Int {
+        simulate(instructions: instructions, registers: registers)
     }
 
-    public func solvePart2() async throws -> Int {
-        0
+    private func simulate(instructions: [Instruction], registers: [String: Value]) -> Int {
+        var registers = registers
+        var last = registers
+        while zRegisters.contains(where: { registers[$0] == .unset }) {
+            for instruction in instructions {
+                if case .set(let lhs) = registers[instruction.lhs], case .set(let rhs) = registers[instruction.rhs] {
+                    registers[instruction.output] = .set(instruction.gate.evaluate(lhs, rhs))
+                }
+            }
+            if last == registers {
+                return -1
+            }
+            last = registers
+        }
+        return value(of: zRegisters, in: registers)
+    }
+
+    public func solvePart2() -> String? {
+        var instructions = instructions
+        var swaps: [String] = []
+        for bit in 0 ..< xRegisters.count where !test(bit: bit, using: instructions) {
+            let candidates = dependencies(of: nameOfZRegister(at: bit + 1), amongst: instructions) + zRegisters.suffix(bit + 1)
+            let swapped = findSwaps(at: bit + 1, amongst: candidates, instructions: &instructions)
+            swaps.append(swapped.0)
+            swaps.append(swapped.1)
+        }
+        return swaps.sorted().joined(separator: ",")
+    }
+
+    private func test(bit: Int, using instructions: [Instruction]) -> Bool {
+        for _ in 1 ... 10 {
+            let x = Int.random(in: 0 ... pow(bit, base: 2))
+            let y = Int.random(in: 0 ... pow(bit, base: 2))
+            var registers = registers
+            setValue(x, to: xRegisters, in: &registers)
+            setValue(y, to: yRegisters, in: &registers)
+            let result = simulate(instructions: instructions, registers: registers)
+            if result != x + y {
+                return false
+            }
+        }
+        return true
+    }
+
+    private func dependencies(of output: String, amongst instructions: [Instruction]) -> Set<String> {
+        let producers = instructions.filter { $0.output == output }
+        if producers.isEmpty {
+            return []
+        } else {
+            var set = Set<String>()
+            for instruction in producers {
+                for input in [instruction.lhs, instruction.rhs] {
+                    if !input.hasPrefix("x") && !input.hasPrefix("y") {
+                        set.insert(input)
+                    }
+                    set.formUnion(dependencies(of: input, amongst: instructions))
+                }
+            }
+            return set
+        }
+    }
+
+    private func findSwaps(at bit: Int, amongst candidates: Set<String>, instructions: inout [Instruction]) -> (String, String) {
+        for (name1, name2) in candidates.sorted().combinations(ofCount: 2).compactMap({ try? $0.elements() }) {
+            let index1 = instructions.firstIndex { $0.output == name1 }!
+            let index2 = instructions.firstIndex { $0.output == name2 }!
+            instructions[index1].output = name2
+            instructions[index2].output = name1
+            if test(bit: bit, using: instructions) {
+                return (name1, name2)
+            }
+            instructions[index1].output = name1
+            instructions[index2].output = name2
+        }
+        fatalError()
+    }
+
+    private func nameOfZRegister(at bit: Int) -> String {
+        "z\(String(format: "%02d", bit))"
+    }
+
+    private func registers(prefixed prefix: String) -> [String] {
+        registers.keys.filter { $0.hasPrefix(prefix) }.sorted().reversed()
+    }
+
+    private func value(of selection: [String], in registers: [String: Value]) -> Int {
+        Int(selection.map { registers[$0]?.value == true ? "1" : "0" }.joined(), radix: 2)!
+    }
+
+    private func setValue(_ value: Int, to selection: [String], in registers: inout [String: Value]) {
+        for name in selection {
+            registers[name] = .set(false)
+        }
+        for (name, value) in zip(selection.reversed(), String(value, radix: 2).reversed()) {
+            registers[name] = .set(value == "1")
+        }
+    }
+}
+
+private struct Instruction {
+    let gate: Gate
+    let lhs: String
+    let rhs: String
+    var output: String
+}
+
+private enum Value: Equatable {
+    case unset
+    case set(Bool)
+
+    var value: Bool? {
+        switch self {
+            case .unset: nil
+            case .set(let value): value
+        }
+    }
+}
+
+private enum Gate: String {
+    case and, or, xor
+
+    func evaluate(_ lhs: Bool, _ rhs: Bool) -> Bool {
+        switch self {
+        case .and: lhs && rhs
+        case .or: lhs || rhs
+        case .xor: lhs != rhs
+        }
     }
 }
--- Tests/2024/Day24.swift
+++ Tests/2024/Day24.swift
@@ -6,34 +6,91 @@ extension CurrentPuzzle: Puzzle {}
 
 private struct Day24 {
     @Test
-    func part1_intro() async throws {
-        let problem = CurrentPuzzle("""
+    func part1_small() throws {
+        let problem = try CurrentPuzzle("""
+        x00: 1
+        x01: 1
+        x02: 1
+        y00: 0
+        y01: 1
+        y02: 0
+        
+        x00 AND y00 -> z00
+        x01 XOR y01 -> z01
+        x02 OR y02 -> z02
         """)
-        try await #expect(problem.solvePart1() == 0)
+        #expect(problem.solvePart1() == 4)
     }
 
     @Test
-    func part2_intro() async throws {
-        let problem = CurrentPuzzle("""
+    func part1_large() throws {
+        let problem = try CurrentPuzzle("""
+        x00: 1
+        x01: 0
+        x02: 1
+        x03: 1
+        x04: 0
+        y00: 1
+        y01: 1
+        y02: 1
+        y03: 1
+        y04: 1
+        
+        ntg XOR fgs -> mjb
+        y02 OR x01 -> tnw
+        kwq OR kpj -> z05
+        x00 OR x03 -> fst
+        tgd XOR rvg -> z01
+        vdt OR tnw -> bfw
+        bfw AND frj -> z10
+        ffh OR nrd -> bqk
+        y00 AND y03 -> djm
+        y03 OR y00 -> psh
+        bqk OR frj -> z08
+        tnw OR fst -> frj
+        gnj AND tgd -> z11
+        bfw XOR mjb -> z00
+        x03 OR x00 -> vdt
+        gnj AND wpb -> z02
+        x04 AND y00 -> kjc
+        djm OR pbm -> qhw
+        nrd AND vdt -> hwm
+        kjc AND fst -> rvg
+        y04 OR y02 -> fgs
+        y01 AND x02 -> pbm
+        ntg OR kjc -> kwq
+        psh XOR fgs -> tgd
+        qhw XOR tgd -> z09
+        pbm OR djm -> kpj
+        x03 XOR y03 -> ffh
+        x00 XOR y04 -> ntg
+        bfw OR bqk -> z06
+        nrd XOR fgs -> wpb
+        frj XOR qhw -> z04
+        bqk OR frj -> z07
+        y03 OR x01 -> nrd
+        hwm AND bqk -> z03
+        tgd XOR rvg -> z12
+        tnw OR pbm -> gnj
         """)
-        try await #expect(problem.solvePart2() == 0)
+        #expect(problem.solvePart1() == 2_024)
     }
 
     @Suite(.tags(.live), .serialized)
-    struct Day24Live {
+    struct Day24Live: @unchecked Sendable {
         private let problem: CurrentPuzzle
         init() async throws {
             problem = try await CurrentPuzzle()
         }
         
         @Test("Day 24 Part 1")
-        func part1() async throws {
-            try await #expect(problem.solvePart1() == 0)
+        func part1() {
+            #expect(problem.solvePart1() == 49_574_189_473_968)
         }
         
         @Test("Day 24 Part 2")
-        func part2() async throws {
-            try await #expect(problem.solvePart2() == 0)
+        func part2() {
+            #expect(problem.solvePart2() == "ckb,kbs,ksv,nbd,tqq,z06,z20,z39")
         }
     }
 }
